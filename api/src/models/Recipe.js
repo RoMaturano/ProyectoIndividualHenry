21f3d89b914d62fb12a8a17a6c8b6094a7c7f799
Rocio <romaturano96@gmail.com>
2023-02-17 22:30:53 -0300

    backListo

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,44 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('Recipe', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    id:{
+      type:DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey:true,
+
+    },
+   
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    summary:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    healthScore:{
+    // (health score) puntaje de salud
+    type: DataTypes.INTEGER,
+    allowNull: false,
+
+    },
+    steps:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdInDb:{
+      type:DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue:true,
+    }
+  },{
+    timestamps: false
+})
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,26 @@
 const { Router } = require('express');
+require('dotenv').config()
+const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+const recipesRouter = require("./recipes.js")
+const dietRouter = require("./diets.js")
+
+
+router.use('/recipes', recipesRouter)
+router.use('/diets', dietRouter)
+
+
+
+
+
+
 
 
 module.exports = router;
